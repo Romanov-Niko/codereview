d3f1d1079c51a87afd026f29fddb72cee2455ffb
Romanov-Niko <55375904+Romanov-Niko@users.noreply.github.com>
2019-12-22 22:18:50 +0200

    Update task3.py

--- romanovmd/second/task3.py
+++ romanovmd/second/task3.py
@@ -2,6 +2,11 @@
 Дан рядок, вивести його задом наперед через одне слово
 """
 
-text = input("Введіть текст - ")
-newtext = text.split(" ")
-print(newtext[::-2])
+def get_text(prompt):
+  text = input(prompt)
+  return text
+def get_reverce():
+  text = get_text("Введіть текст - ")
+  newtext = text.split()
+  return newtext[::-2]
+print(get_reverce())
