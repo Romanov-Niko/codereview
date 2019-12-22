0bbec76b1e6de2807e3759ffa57bd16678328db6
Romanov-Niko <55375904+Romanov-Niko@users.noreply.github.com>
2019-12-22 22:16:07 +0200

    Update task1.py

--- romanovmd/second/task1.py
+++ romanovmd/second/task1.py
@@ -2,9 +2,11 @@
 Порахувати кількість дробових чисел в списку
 """
 
+import re
+
 list = ["dfg", 1.4, 1.3, 6.6, "f", 8.7]
 count = 0
 for i in list:
-    if isinstance(i, float):
+    if re.match("^\d+.\d*[123456789]\d*$",str(i)):
         count = count + 1
 print(count)
